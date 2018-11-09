f727861f20552e5525735aff3413028f4a2c1f68
bensir213 <573603568@qq.com>
2018-11-09 10:27:43 +0800

    first commit

---
+++ src/main/java/com/api/main/MainTest.java
@@ -0,0 +1,11 @@
+package com.api.main;
+
+import org.apache.log4j.Logger;
+
+public class MainTest {
+    private static final Logger logger = Logger.getLogger(MainTest.class.getName());
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/api/run/TestRunner.java
@@ -0,0 +1,23 @@
+package com.api.run;
+
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class TestRunner {
+
+    @BeforeTest
+    public void beforeRun() {
+
+    }
+
+    @Test
+    public void run() {
+
+    }
+
+    @AfterTest
+    public void afterRun() {
+
+    }
+}
